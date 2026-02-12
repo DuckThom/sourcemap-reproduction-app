5422fdb56272ea244ed9c4e28ebe398e93a3f27f
Thomas Wiringa <git@tjwiringa.dev>
2026-02-12 09:59:08 +0100

    explicitly disable sourcemaps

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+
+    // Explicitly disable sourcemaps
+    productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
